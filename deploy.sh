751657211c3d784cb8a9fb7525d87f25c9476b89
Arwan Khoiruddin <arwan.khoiruddin@gmail.com>
2018-01-25 11:21:16 +0800

    added namenode datanode and temporary config

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+hdpath="../hadoop-3.0.0"
+hdtarget="../coba"
+hdfspath="~/hdfs"
+
+# make datanode, namenode and temporary directories
+mkdir "$hdfspath/datanode"
+mkdir "$hdfspath/namenode"
+mkdir "$hdfspath/tmp"
