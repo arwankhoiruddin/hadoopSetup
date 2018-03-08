2ee5fc1673547cc7f046a74226ec4edb25baf9a4
Arwan Khoiruddin <arwan.khoiruddin@gmail.com>
2018-03-08 14:44:05 +0800

    updated with confirmations

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,18 @@ sudo apt-get install oracle-java8-installer
 
 # update ~/.bashrc
 cp ./bashrc ~/.bashrc
+echo "bashrc updated"
 
 # disable ipv6
 sudo cp ./sysctl.conf /etc/sysctl.conf
 sudo sysctl -p
+echo "ipv6 disabled"
 
 # make folders for datanode, namenode and temporary
 mkdir -p /home/hduser/hdfs/tmp
 mkdir -p /home/hduser/hdfs/datanode
 mkdir -p /home/hduser/hdfs/namenode
+echo "directories needed by Hadoop are created"
 
 # copy files into the desired target
 echo "now you can edit and run ./copyconf.sh to copy the configurations into your hadoop"
