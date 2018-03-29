174a4e4b20988515f5fc62d7da41ec824686023e
arwan khoriuddin <arwan.khoiruddin@mgmail.com>
2018-03-29 12:58:41 +0000

    added copyssh.sh

---
+++ copyssh.sh
@@ -0,0 +1,5 @@
+ssh-keygen -t rsa -P ""
+ssh-copy-id -i ~/.ssh/id_rsa.pub hduser@d1
+ssh-copy-id -i ~/.ssh/id_rsa.pub hduser@d2
+ssh-copy-id -i ~/.ssh/id_rsa.pub hduser@d3
+ssh-copy-id -i ~/.ssh/id_rsa.pub hduser@d4
