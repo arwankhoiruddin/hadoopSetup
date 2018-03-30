33309ee2d1ef7f326a30bb7afc9362901ec4bd8e
arwan khoiruddin <arwan.khoiruddin@gmail.com>
2018-03-30 14:39:32 +0800

    updated prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,13 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install software-properties-common oracle-java8-installer ansible git aptitude firefox openssh-server
 
+sudo useradd -c "Hadoop user" hduser
+sudo passwd hduser
+usermod -aG sudo hduser
+
+su hduser
+mkdir -P /home/hduser/.ssh
+
 mkdir riset
 cd riset
 curl -O https://gist.githubusercontent.com/arwankhoiruddin/b913b4d87f7ec9617016bc28c9e531a7/raw/5dde11bbdc3161717f07cc73535c5d1fc4b6f983/customDockerNetworkVolume.py
@@ -17,13 +24,3 @@ cd ..
 sudo python setup.py install
 
 sudo service ssh restart
-
-sudo useradd -c "Hadoop user" hduser
-sudo passwd hduser
-
-usermod -aG sudo hduser
-
-mkdir -P /home/hduser/.ssh
-sudo chown -R hduser:hduser /home/hduser/.ssh
-
-su hduser
