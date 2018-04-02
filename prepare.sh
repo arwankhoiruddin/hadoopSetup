b66166eed0191a3c566565edd3c7553f4995d8ae
Arwan Ahmad Khoiruddin <arwan.khoiruddin@gmail.com>
2018-04-02 22:48:22 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -6,6 +6,7 @@ mkdir riset
 cd riset
 curl -O https://gist.githubusercontent.com/arwankhoiruddin/b913b4d87f7ec9617016bc28c9e531a7/raw/5dde11bbdc3161717f07cc73535c5d1fc4b6f983/customDockerNetworkVolume.py
 mv customDockerNetworkVolume.py mynet.py
+chmod +x mynet.py
 
 git clone https://github.com/containernet/containernet.git
 cd containernet/ansible
