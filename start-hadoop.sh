8fae9f967efff0f012b4adc52a766302bd89ba08
Arwan Ahmad Khoiruddin <arwan.khoiruddin@gmail.com>
2018-06-07 18:09:22 +0800

    Delete start-hadoop.sh

--- start-hadoop.sh
+++
@@ -1,6 +0,0 @@
-echo 'e' > $HADOOP_HOME/etc/hadoop/masters
-hadoop namenode -format
-$HADOOP_HOME/sbin/start-dfs.sh
-$HADOOP_HOME/sbin/start-yarn.sh
-$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh --config $HADOOP_HOME/etc/hadoop start historyserver
-jps
