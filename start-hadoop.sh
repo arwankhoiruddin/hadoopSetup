88df577390b509254e84d545597b4e750fa86ffe
Arwan Ahmad Khoiruddin <arwan.khoiruddin@gmail.com>
2018-06-07 17:52:55 +0800

    Update start-hadoop.sh

--- start-hadoop.sh
+++ start-hadoop.sh
@@ -3,3 +3,4 @@ hadoop namenode -format
 $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
 $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh --config $HADOOP_HOME/etc/hadoop start historyserver
+jps
