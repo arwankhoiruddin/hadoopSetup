d07a12dae72e9ba1e025ac30ae0778118e597c21
Arwan Ahmad Khoiruddin <arwan.khoiruddin@gmail.com>
2018-06-07 17:52:24 +0800

    Create start-hadoop.sh

---
+++ start-hadoop.sh
@@ -0,0 +1,5 @@
+echo 'e' > $HADOOP_HOME/etc/hadoop/masters
+hadoop namenode -format
+$HADOOP_HOME/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-yarn.sh
+$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh --config $HADOOP_HOME/etc/hadoop start historyserver
