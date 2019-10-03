5522d9d2293ca3bf574907810ae684d6b839e442
Arwan Ahmad Khoiruddin <arwan.khoiruddin@gmail.com>
2019-10-03 23:35:11 +0800

    Update copysshAndStartHadoop.sh

--- copysshAndStartHadoop.sh
+++ copysshAndStartHadoop.sh
@@ -1,4 +1,5 @@
 ssh-keygen -t rsa -P ""
+ssh-copy-id -i ~/.ssh/id_rsa.pub hduser@d0
 ssh-copy-id -i ~/.ssh/id_rsa.pub hduser@d1
 ssh-copy-id -i ~/.ssh/id_rsa.pub hduser@d2
 ssh-copy-id -i ~/.ssh/id_rsa.pub hduser@d3
@@ -7,7 +8,7 @@ ssh 0.0.0.0
 
 echo 'starting hadoop now'
 
-echo 'e' > $HADOOP_HOME/etc/hadoop/masters
+echo 'd0' > $HADOOP_HOME/etc/hadoop/masters
 hadoop namenode -format
 $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
