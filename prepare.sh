3359e30f1655b3a9f386fa2a658476583f699b0e
Arwan Ahmad Khoiruddin <arwan.khoiruddin@gmail.com>
2019-10-03 16:27:32 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,11 @@ sudo apt-get install software-properties-common ansible git aptitude firefox ope
 sudo mkdir /usr/lib/jvm
 cd /usr/lib/jvm
 sudo tar -xvzf /root/hadoopSetup/jdk-8u221-linux-x64.tar.gz
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_221/bin/java" 0
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0_221/bin/javac" 0
+sudo update-alternatives --set java /usr/lib/jvm/jdk1.8.0_221/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/jdk1.8.0_221/bin/javac
+
 
 cd ..
 mkdir research
