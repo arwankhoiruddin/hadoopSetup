44a0157704048dcf6eebb09fdfd19b5c24e55beb
Arwan Ahmad Khoiruddin <arwan.khoiruddin@gmail.com>
2019-10-03 12:42:53 +0800

    oracle now needs to login to download the jdk

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,10 @@
-sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install software-properties-common oracle-java8-installer ansible git aptitude firefox openssh-server
+sudo apt-get install software-properties-common ansible git aptitude firefox openssh-server
 
 cd ..
 mkdir research
 cd research
-curl -O https://gist.githubusercontent.com/arwankhoiruddin/36a2bc0b30c60a4101ebed0e6f5d0c3d/raw/ebea5d06fc7e02fccbb72825f4fe1173417089c1/singleRackContainernet.py
+curl -O https://gist.githubusercontent.com/arwankhoiruddin/36a2bc0b30c60a4101ebed0e6f5d0c3d/raw/ac1f8cb11e46c9496da56796ea733186430780a4/singleRackContainernet.py
 mv singleRackContainernet.py mynet.py
 chmod +x mynet.py
 
