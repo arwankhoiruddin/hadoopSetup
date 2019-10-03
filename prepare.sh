2403470dc574d574ccebedd07667c15d255f7032
Arwan Ahmad Khoiruddin <arwan.khoiruddin@gmail.com>
2019-10-03 16:48:44 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -32,7 +32,7 @@ cd ..
 sudo python setup.py install
 
 cd ..
-curl -O http://www-us.apache.org/dist/hadoop/common/hadoop-2.9.1/hadoop-2.9.1.tar.gz
+curl -O https://archive.apache.org/dist/hadoop/core/hadoop-2.9.1/hadoop-2.9.1.tar.gz
 tar xvfz hadoop-2.9.1.tar.gz
 mv hadoop-2.9.1 hd29
 rm hadoop-2.9.1.tar.gz
